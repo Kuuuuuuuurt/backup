aa4931180837acce7b5741b2288e391380935441
kuuuuuuuurt <kurtvonn98@gmail.com>
2022-04-01 15:21:32 +0800

    first commit

---
+++ src/views/users/individual/GenerateQR.vue
@@ -0,0 +1,551 @@
+<template>
+  <div class="flex flex-wrap items-center justify-center -mt-5">
+    <div class="w-full md:w-3/5 bg-white p-6">
+      <div class="mt-5 text-2xl">
+        <p class="text-center">
+          <span class="">Setup QR Information</span>
+        </p>
+      </div>
+      <div class="flex items-center border-b py-4"></div>
+      <section class="antialiased text-gray-600 h-screen px-2">
+        <div class="flex flex-col justify-center">
+          <!-- Table -->
+          <div
+            class="
+              w-full
+              max-w-2xl
+              mx-auto
+              bg-white
+              shadow-lg
+              rounded-sm
+              border border-gray-200
+              my-5
+            "
+          >
+            <header class="px-5 py-4 border-b border-gray-100">
+              <h2 class="font-semibold text-gray-800">Personal Information:</h2>
+            </header>
+            <div class="p-3">
+              <div class="overflow-x-auto inline-block">
+                <div class="flex mt-2 items-center">
+                  <div class="text-gray-400">
+                    <img
+                      class="w-5"
+                      src="https://cdn-icons-png.flaticon.com/512/1077/1077063.png"
+                      alt="logo"
+                    />
+                    <i class="fas fa-home"></i>
+                  </div>
+
+                  <div class="text-lg ml-3">
+                    <p>
+                      <span class="font-bold">{{ this.$data.name }}</span>
+                    </p>
+                  </div>
+                </div>
+
+                <div class="flex mt-2 items-center">
+                  <div class="text-gray-400">
+                    <img
+                      class="w-5"
+                      src="https://cdn-icons-png.flaticon.com/512/684/684809.png"
+                      alt="logo"
+                    />
+                    <i class="fas fa-home"></i>
+                  </div>
+
+                  <div class="text-lg ml-3">
+                    <p>
+                      Lives in
+                      <span class="font-bold">{{ this.$data.address }}</span>
+                    </p>
+                  </div>
+                </div>
+
+                <div class="flex mt-2 items-center">
+                  <div class="text-gray-400">
+                    <img
+                      class="w-4"
+                      src="https://cdn-icons-png.flaticon.com/512/159/159832.png"
+                      alt="logo"
+                    />
+                  </div>
+
+                  <div class="text-lg ml-3">
+                    <p>{{ this.$data.contactNumber }}</p>
+                  </div>
+                </div>
+
+                <div class="flex mt-2 items-center">
+                  <div class="text-gray-400">
+                    <img
+                      class="w-4"
+                      src="https://cdn-icons-png.flaticon.com/512/1077/1077063.png"
+                      alt="logo"
+                    />
+                  </div>
+
+                  <div class="text-lg ml-3">
+                    <p>{{ this.$data.gender }}</p>
+                  </div>
+                </div>
+
+                <div class="flex mt-2 items-center">
+                  <div class="text-gray-400">
+                    <img
+                      class="w-4"
+                      src="https://cdn-icons-png.flaticon.com/512/1077/1077063.png"
+                      alt="logo"
+                    />
+                  </div>
+
+                  <div class="text-lg ml-3">
+                    <p>{{ this.$data.age }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- vaxx card -->
+        <div
+          class="
+            w-full
+            max-w-2xl
+            mx-auto
+            bg-white
+            shadow-lg
+            rounded-sm
+            border border-gray-200
+            my-5
+          "
+        >
+          <header class="px-5 py-4 border-b border-gray-100">
+            <h2 class="font-semibold text-gray-800">Vaccination Card</h2>
+          </header>
+          <div class="p-3">
+            <div class="overflow-x-auto inline-block"></div>
+            <div>
+              <img :src="vaccinationCard"/>
+            </div>
+
+            <div>
+              <label
+                class="
+                  block
+                  text-sm
+                  font-medium
+                  text-gray-900
+                  dark:text-gray-300
+                "
+                for="vaccination_cards"
+                >Upload Image</label
+              >
+              <input
+                class="
+                  block
+                  w-full
+                  text-sm text-gray-900
+                  bg-gray-50
+                  rounded-lg
+                  border border-gray-300
+                  cursor-pointer
+                  dark:text-gray-400
+                  focus:outline-none focus:border-transparent
+                  dark:bg-gray-700
+                  dark:border-gray-600
+                  dark:placeholder-gray-400
+                "
+                aria-describedby="vaccination_card"
+                id="vaccination_cards"
+                type="file"
+                accept="image/*"
+                @change="previewVaxx"
+              />
+              <div
+                class="mt-1 text-sm text-gray-500 dark:text-gray-300"
+                id="vaccination_card"
+              >
+                Vaccination Card Captured in Landscape
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+          <div
+          class="
+            w-full
+            max-w-2xl
+            mx-auto
+            bg-white
+            shadow-lg
+            rounded-sm
+            border border-gray-200
+            my-5
+          "
+        >
+
+          <header class="px-5 py-4 border-b border-gray-100">
+            <h2 class="font-semibold text-gray-800">Valid ID</h2>
+          </header>
+
+            <div class="p-3">
+            <div class="overflow-x-auto inline-block"></div>
+            <div>
+              <img :src="validID" class="preview"/>
+            </div>
+
+            <div>
+              <label
+                class="
+                  block
+                  text-sm
+                  font-medium
+                  text-gray-900
+                  dark:text-gray-300
+                "
+                for="validID_cards"
+                >Upload Image</label
+              >
+              <input
+                class="
+                  block
+                  w-full
+                  text-sm text-gray-900
+                  bg-gray-50
+                  rounded-lg
+                  border border-gray-300
+                  cursor-pointer
+                  dark:text-gray-400
+                  focus:outline-none focus:border-transparent
+                  dark:bg-gray-700
+                  dark:border-gray-600
+                  dark:placeholder-gray-400
+                "
+                aria-describedby="validID"
+                id="validID_cards"
+                type="file"
+                accept="image/*"
+                @change="previewValidID"
+              />
+              <div
+                class="mt-1 text-sm text-gray-500 dark:text-gray-300"
+                id="vaccination_card"
+              >
+                Valid ID Captured in Landscape
+              </div>
+            </div>
+
+            </div>
+
+          </div>
+        <div class="flex pb-4 mt-4 items-center border-b flex w-48">
+          <button
+            class="py-2 rounded flex-1 bg-green-500 text-white"
+            @click="submitData"
+          >
+            <i class="fas fa-plus-circle"></i> Submit
+          </button>
+          <button
+            class="ml-1 py-2 rounded flex-1 bg-gray-400 text-white"
+            @click="cancel"
+          >
+            <i class="fas fa-plus-circle"></i> Back
+          </button>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import app from "../../../firebase/auth-individual/firebase";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+} from "firebase/firestore";
+
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+export default {
+  data() {
+    return {
+      vaccinationCard: null,
+      vaccinationData: null,
+      validID: null,
+      validIDData: null,
+      userID: null,
+      userRefer: null,
+      name: "",
+      gender: "",
+      age: "",
+      address: "",
+      contactNumber: "",
+      user: {
+        userInfo: {
+          phoneNumber: "",
+          firstName: "",
+          lastName: "",
+          gender: "",
+          age: "",
+          municipality: "",
+          baranggay: "",
+          purok: "",
+          qrStatus: "Pending Application",
+          qrData: null,
+          type: "individual",
+          vaccinationLink: null,
+          validIdLink: null,
+        },
+      },
+    };
+  },
+  methods: {
+    async getUser() {
+      console.log("generator");
+      console.log(this.userID);
+
+      const db = getFirestore(app);
+      const userRef = collection(db, "user");
+
+      let usersRef = doc(userRef, this.userID);
+      this.$data.userRefer = usersRef;
+
+      let user = await getDoc(this.$data.userRefer);
+
+      let userData = user.data();
+
+      console.log(userData);
+
+      this.$data.name =
+        userData.userInfo.firstName + " " + userData.userInfo.lastName;
+      this.$data.address =
+        userData.userInfo.purok +
+        ", " +
+        userData.userInfo.baranggay +
+        ", " +
+        userData.userInfo.municipality;
+      this.$data.age = userData.userInfo.age + " Years Old";
+      this.$data.gender = userData.userInfo.gender;
+      this.$data.contactNumber = "+63" + userData.userInfo.phoneNumber;
+
+      // data setting for collection
+      this.$data.user.userInfo.phoneNumber = userData.userInfo.phoneNumber;
+      this.$data.user.userInfo.firstName = userData.userInfo.firstName;
+      this.$data.user.userInfo.lastName = userData.userInfo.lastName;
+      this.$data.user.userInfo.age = userData.userInfo.age;
+      this.$data.user.userInfo.gender = userData.userInfo.gender;
+      this.$data.user.userInfo.baranggay = userData.userInfo.baranggay;
+      this.$data.user.userInfo.municipality = userData.userInfo.municipality;
+      this.$data.user.userInfo.purok = userData.userInfo.purok;
+    },
+
+    cancel() {
+      this.$router.push(`/individual-home/${this.$data.userID}`);
+    },
+
+    submitData() {
+      var randomstring = require("randomstring");
+      let id = randomstring.generate({
+        length: 30,
+        charset: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      });
+      console.log(id);
+
+      const db = getFirestore(app);
+      const addDocs = setDoc(doc(db, "qr-application", id), this.$data.user);
+
+      const addDocu = setDoc(
+        doc(db, "user", this.$data.userID),
+        this.$data.user
+      );
+      console.log(addDocu);
+
+      console.log(addDocs);
+      alert("Application for Qr-Code Submitted, Please wait for the response!");
+      this.$router.push(`/individual-home/${this.$data.userID}`);
+    },
+
+    previewVaxx(event) {
+      var input = event.target;
+
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = () => {
+          this.vaccinationData = event.target.files[0];
+          this.uploadVaccineCard();
+        };
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    },
+
+    previewValidID(event){
+      var input = event.target;
+
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = () => {
+          this.validIDData = event.target.files[0];
+          this.uploadValidID();
+        };
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    },
+
+
+    uploadVaccineCard(){
+      const storage = getStorage(app);
+
+           // Create the file metadata
+      /** @type {any} */
+      const metadata = {
+        contentType: "image/jpeg",
+      };
+      // Upload file and metadata to the object 'images/mountains.jpg'
+      const storageRef = ref(
+        storage,
+        "proofs/" + `${this.$data.contactNumber}/` + "vaccinationCard/" + this.vaccinationData.name
+      );
+      const uploadTask = uploadBytesResumable(
+        storageRef,
+        this.vaccinationData,
+        metadata
+      );
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+
+            // ...
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            this.vaccinationCard = downloadURL;
+            this.$data.user.userInfo.vaccinationLink = downloadURL;
+            console.log("Link:" + this.$data.user.userInfo.vaccinationLink);
+          });
+        }
+      );
+    },
+
+    uploadValidID(){
+      const storage = getStorage(app);
+
+           // Create the file metadata
+      /** @type {any} */
+      const metadata = {
+        contentType: "image/jpeg",
+      };
+      // Upload file and metadata to the object 'images/mountains.jpg'
+      const storageRef = ref(
+        storage,
+        "proofs/" + `${this.$data.contactNumber}/` + "ID/" + this.validIDData.name
+      );
+      const uploadTask = uploadBytesResumable(
+        storageRef,
+        this.validIDData,
+        metadata
+      );
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+
+            // ...
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            this.validID = downloadURL;
+            this.$data.user.userInfo.validIdLink = downloadURL;
+            console.log("Link:" + this.$data.user.userInfo.validIdLink);
+          });
+        }
+      );
+    },
+
+
+  },
+
+  created() {
+    let id = this.$route.params.phoneId;
+    this.$data.userID = id;
+    this.getUser();
+  },
+};
+</script>
